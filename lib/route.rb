ae46847f7fa659dab6054f130a9887b913659f44
BenWix <44454993+BenWix@users.noreply.github.com>
2020-04-13 11:43:02 -0400

    Removed unnecessary code

--- lib/route.rb
+++ lib/route.rb
@@ -24,19 +24,6 @@ class Route
     def self.all_ids
         @route_ids = all.map{|route| route.id}
     end
-    
-    # def self.new_from_array(routes, location) 
-    #     routes.each do |route|
-    #         name = route["name"] 
-    #         grade = route["rating"] 
-    #         type = route["type"] 
-    #         stars = route["stars"]
-    #         id = route["id"]
-    #         unless all_ids.include?(id)
-    #             Route.new(name, grade, type, stars, id, location)
-    #         end 
-    #     end 
-    # end 
 
     def self.new_from_hash(route_hash, location)
         name = route_hash["name"] 
@@ -47,10 +34,7 @@ class Route
         Route.new(name, grade, type, stars, id, location)
     end 
         
-    def self.get_routes_by_location(location)
-        selected_routes = Route.all.select{|route| route.location == location}
-        # selected_routes.all.each.with_index(1) do |route, index|
-        #     puts "#{index}.#{route.name}: grade #{route.grade}, type #{route.type}. stars #{route.stars}"
-        # end 
-    end
+    # def self.get_routes_by_location(location)
+    #     selected_routes = Route.all.select{|route| route.location == location}
+    # end
 end 
